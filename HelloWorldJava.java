aca2877c75d413579ed461615b24ed1264d6fa5f
Justin Jon Sarmiento <91234787+TinJon06@users.noreply.github.com>
2021-09-23 11:22:48 +0800

    Hello World program of Java
    This is an Hello World program of Java. Run it on a Java IDE, make sure you have Java installed in your computer.

---
+++ HelloWorldJava.java
@@ -0,0 +1,9 @@
+package helloworldjava;
+
+public class HelloWorldJava {
+
+    public static void main(String[] args) {
+       System.out.println("Hello World!"); // This statement displays "Hello World!" on console.
+    }
+    
+}
