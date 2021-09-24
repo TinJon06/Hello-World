d09b0852908c8be2babb2cf2b2b23a42166a52aa
Justin Jon Sarmiento <91234787+TinJon06@users.noreply.github.com>
2021-09-24 18:56:26 +0800

    Hello World Program for C#
    This is a Hello World Program for C#. Run it on a C# IDE, Visual Studio/Visual Studio Code with Code Runner recommended.

---
+++ HelloWorld.cs
@@ -0,0 +1,7 @@
+using System;
+
+class HelloWorld {
+    static void Main(string[] args){
+        Console.WriteLine("Hello World!"); // WriteLine prints a string or value into a new line.
+    }
+}
